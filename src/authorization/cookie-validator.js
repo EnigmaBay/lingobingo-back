c91061f269aaf4699bc34802b7bf036bf5820b39
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-01-13 07:15:12 -0800

    implemented cookie validator, working on local.

--- src/authorization/cookie-validator.js
+++ src/authorization/cookie-validator.js
@@ -1,21 +1,33 @@
-function cookieValidator(useruuid) {
+function cookieValidator(req, res, next) {
+  res.locals.cookieResult = false;
+  res.status(500);
 
-  if (useruuid === undefined) {
-    return false;
+  // test if presenterUuid is set and if not then find or create Presenter failed so do not set new cookies
+  try {
+    if (res.locals.presenterUuid !== undefined) {
+      const userUuid = res.locals.presenterUuid;
+      const uuidMatches = [...userUuid.matchAll(/([g-zG-Z])/g)];
+
+      // hex values do not include alpha characters g through z
+      if (uuidMatches.length === 0) {
+
+        if (userUuid.split("").length > 29) {
+          res.cookie("useruuid", userUuid, {
+            maxAge: process.env.MAX_COOKIE_AGE,
+          });
+          res.cookie("username", req.user.given_name, {
+            maxAge: process.env.MAX_COOKIE_AGE,
+          });
+          res.status(200);
+          res.locals.cookieResult = true;
+        }
+      }
+    }
+  } catch (error) {
+    console.log("cookieValidate catch block error:", error);
+    res.status(500);
+    res.locals.cookieResult = false;
   }
-  const uuidMatches = [...useruuid.matchAll(/([g-zG-Z])/g)];
-
-  if (uuidMatches.length > 0) {
-    return false;
-  }
-
-  if (useruuid.split('').length < 30) {
-    return false;
-  }
-
-  // since authentication is handled on the front-end this module
-  // avoids storing or passing user information other than uuid
-  return true;
 }
 
 module.exports = cookieValidator;
