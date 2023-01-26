9679fab9e4e3c83f3f8b473f2a35a2dc2e0051b1
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-01-26 15:55:14 -0800

    refactored validate-inputs.

--- src/utils/validate-inputs.js
+++ src/utils/validate-inputs.js
@@ -1,17 +1,45 @@
 function checkString(item) {
-  if (typeof item === 'undefined') {
+  if (typeof item === "undefined") {
     return undefined;
   }
 
-  if (typeof item === 'string') {
+  if (typeof item === "string") {
     if (item.length < 1) {
       return undefined;
     } else {
       return item.trim();
     }
   }
-
-  // TODO: consider other necessary input validators and cleaners  
 }
 
-module.exports = checkString;
+function validateInputs(useruuid, username) {
+  console.log("cookie-validator validateInputs received useruuid, username");
+
+  if (useruuid === undefined) {
+    return false;
+  }
+
+  const uuidMatches = [...useruuid.matchAll(/([g-zG-Z])/g)];
+
+  if (uuidMatches.length > 0) {
+    return false;
+  }
+
+  if (useruuid.split("").length < 30) {
+    return false;
+  }
+
+  if (username === undefined) {
+    return false;
+  }
+
+  const usernameMatches = [...username.matchAll(/([a-zA-Z])/g)];
+
+  if (usernameMatches.length < 1) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = { checkString, validateInputs };
