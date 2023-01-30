214687ae7c82615582b8dc385b20cec35a1aa35e
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-01-30 15:57:40 -0800

    added 24-word requirement to create a gameboard.

--- src/route-handlers/create-gameboard.js
+++ src/route-handlers/create-gameboard.js
@@ -1,5 +1,6 @@
 const { checkString } = require("../utils/validate-inputs");
 const generateUuid = require("../utils/gameboard-uuid-generator");
+const getWords = require("./get-words");
 
 async function createGameboard(req, res, next) {
   // validate inputs
@@ -24,6 +25,8 @@ async function createGameboard(req, res, next) {
       foundGameboard
     );
 
+    let words;
+
     if (!foundGameboard) {
       const concatString = `${presenterUuid}${cat}`;
       gameboardUuid = generateUuid(concatString);
@@ -35,20 +38,27 @@ async function createGameboard(req, res, next) {
         category: category,
       });
     } else {
-      gameboardUuid = foundGameboard["uuid"];
+      words = await getWords(ownerUuid, category);
+      if (words.count > 23) {
+        gameboardUuid = foundGameboard["uuid"];
+      } else {
+        console.log(
+          "create-gameboard: not enough words in category!",
+          words.count
+        );
+        throw new Error("Not eough words in category.");
+      }
     }
   } catch (error) {
     console.log("create-gameboard threw error:", error.message);
-    res.locals.statusCode = 500;
-    res.locals.resultMsg = "Unable to complete request.";
+    res.locals.statusCode = 400;
+    res.locals.resultMsg =
+      "Need at least 24 words in a category to create a board.";
   }
 
   // return gameboard UUID
   res.locals.gameboardUuid = gameboardUuid;
-  console.log(
-    "create-gameboard: gameboardUuid is",
-    res.locals.gameboardUuid
-  );
+  console.log("create-gameboard: gameboardUuid is", res.locals.gameboardUuid);
   next();
 }
 
