17c250202070e6c984cdce55e9f408f147fcbb78
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-01-31 15:18:06 -0800

    removed nodemode from server.js and dotenv.

--- src/server.js
+++ src/server.js
@@ -9,8 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.disable("x-powered-by");
 
-console.log("node mode:", process.env.NODE_ENV);
-
 app.get("/", (req, res) => {
   res.status(200).json({ message: "ehlo werld!" });
 });
