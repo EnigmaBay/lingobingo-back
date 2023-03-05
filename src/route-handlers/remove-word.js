9cdd7185f44c1597bf0324369b1ef38d78b242ee
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-03-04 19:34:42 -0800

    remove-word better handles errors now.

--- src/route-handlers/remove-word.js
+++ src/route-handlers/remove-word.js
@@ -2,15 +2,14 @@ const LingoWord = require("../models/lingowordModel");
 const { checkString } = require("../utils/validate-inputs");
 const timeStamper = require("../utils/time-stamper");
 
-async function deleteWord(req, res, category, wordToDelete) {
+async function deleteWord(req, res, next) {
   const owner = checkString(req.cookies["useruuid"]);
-  const cat = checkString(category);
-  const word = checkString(wordToDelete);
+  const cat = checkString(res.locals.category);
+  const word = checkString(res.locals.word);
 
   if (!owner || !cat || !word) {
     res.locals.statusCode = 400;
-    res.locals.resultMsg = "Invalid request";
-    return word;
+    next("Invalid request");
   }
 
   try {
@@ -30,31 +29,35 @@ async function deleteWord(req, res, category, wordToDelete) {
     );
 
     console.log("remove-word update db statistics:", updatedItem);
+    let resultMsg;
 
     if (updatedItem["modifiedCount"] === 1) {
       resultMsg = "deleted.";
-      statusCode = 200;
-    }
+      res.locals.statusCode = 200;
+      console.log('remove-word: status code: 200');
+    } else {
 
     if (updatedItem["modifiedCount"] > 1) {
       resultMsg = "multiple deleted.";
-      statusCode = 500;
-    }
-
-    if (updatedItem["modifiedCount"] < 1) {
+      res.locals.statusCode = 500;
+      console.log('remove-word: status code: 500');
+    } else {
+      // updatedItem["modifiedCount"] < 1
       resultMsg = "not found.";
-      statusCode = 404;
+      res.locals.statusCode = 404;
+      console.log('remove-word: status code: 404');
     }
 
-    res.locals.resultMsg = resultMsg;
-    res.locals.statusCode = statusCode;
-  } catch (error) {
-    console.log(error.message);
-    res.locals.statusCode = 500;
-    res.locals.resultMsg = "Error";
+    console.log('remove-word calling next with resultMsg:', resultMsg);
+    next(resultMsg);
   }
 
-  return word;
+    res.locals.resultMsg = resultMsg;
+  } catch (error) {
+    console.log('error.message:', error.message);
+    res.locals.statusCode = 500;
+    next(error.message);
+  }
 }
 
 module.exports = deleteWord;
