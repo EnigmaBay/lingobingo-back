7d9db82cdc5dd7683d92c174006b635f4f2b44db
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-17 21:34:13 -0700

    linted, debugged updadd-word.

--- src/route-handlers/updadd-word.js
+++ src/route-handlers/updadd-word.js
@@ -2,16 +2,24 @@
 // or add new if it does not already exist
 // and will reset deleted field appropriately
 
-const LingoWord = require("../models/lingowordModel");
-const { checkString } = require("../utils/validate-inputs");
-const timeStamper = require("../utils/time-stamper");
+const LingoWord = require('../models/lingowordModel');
+const { checkString } = require('../utils/validate-inputs');
+const timeStamper = require('../utils/time-stamper');
 
 async function updaddWord(req, res, next) {
-  const owner = checkString(req.cookies["useruuid"]);
+  const owner = checkString(res.locals.presenterUuid);
   const category = checkString(res.locals.category);
   const word = checkString(res.locals.word);
   const newWord = checkString(res.locals.newWord);
   const timeStamp = timeStamper();
+  console.log(
+    'owner, category, word, newWord, timestamp:',
+    owner,
+    category,
+    word,
+    newWord,
+    timeStamp
+  );
 
   try {
     const findWord = await LingoWord.findOne({
@@ -19,24 +27,25 @@ async function updaddWord(req, res, next) {
       category: category,
       owner: owner,
     }).exec();
-    console.log("updadd-word findWord:", findWord);
+    console.log('updadd-word findWord:', findWord);
 
     if (word === newWord) {
+      console.log('word equals newWord so this is an add operation.');
       // create new word or swap 'deleted' flag to true
       if (findWord) {
-        if (findWord["deleted"] === true) {
+        if (findWord['deleted'] === true) {
           // just flip the flag to false
-          findWord["deleted"] = false;
-          findWord["updated"] = timeStamp;
+          findWord['deleted'] = false;
+          findWord['updated'] = timeStamp;
           findWord.save();
           // DONE
-          console.log("updadd-word flipped deleted flag to false, word:", word);
+          console.log('updadd-word flipped deleted flag to false, word:', word);
           res.locals.statusCode = 201;
           res.locals.resultMsg = word;
         } else {
           // no work to do word exists return http 200
           console.log(
-            "updadd-word: word equals new word, word was found, and deleted flag is false."
+            'updadd-word: word equals new word, word was found, and deleted flag is false: No work to do.'
           );
           res.locals.statusCode = 200;
           res.locals.resultMsg = word;
@@ -47,7 +56,7 @@ async function updaddWord(req, res, next) {
           category: category,
           owner: owner,
         });
-        console.log("updadd-word addedItem:", addedItem);
+        console.log('updadd-word addedItem:', addedItem);
         res.locals.statusCode = 201;
         res.locals.newWord = word;
       }
@@ -58,31 +67,31 @@ async function updaddWord(req, res, next) {
         category: category,
         owner: owner,
       });
-      console.log("updadd-word findNewWord:", findNewWord);
+      console.log('updadd-word findNewWord:', findNewWord);
 
       if (findWord) {
         // word exists
         if (findNewWord) {
           // newWord ALSO exists so update them BOTH
-          findWord["deleted"] = true;
-          findWord["updated"] = timeStamp;
+          findWord['deleted'] = true;
+          findWord['updated'] = timeStamp;
           await findWord.save();
-          findNewWord["deleted"] = false;
-          findNewWord["updated"] = timeStamp;
+          findNewWord['deleted'] = false;
+          findNewWord['updated'] = timeStamp;
           await findNewWord.save();
           console.log(
-            "updadd-word found word and new word so flipped deleted flags accordingly."
+            'updadd-word found word and new word so flipped deleted flags accordingly.'
           );
           res.locals.statusCode = 201;
           res.locals.newWord = newWord;
         } else {
-          // newWord does NOT exist to just update word
-          findWord["word"] = newWord;
-          findWord["deleted"] = false;
-          findWord["updated"] = timeStamp;
+          // but newWord does NOT exist so just update word
+          findWord['word'] = newWord;
+          findWord['deleted'] = false;
+          findWord['updated'] = timeStamp;
           await findWord.save();
           console.log(
-            "updadd-word found word but not new word so just updated word to:",
+            'updadd-word found word but not new word so just updated word to:',
             findWord
           );
           res.locals.statusCode = 201;
@@ -92,10 +101,10 @@ async function updaddWord(req, res, next) {
         // word NOT found
         if (findNewWord) {
           // but newWord exists just mark deleted false and return
-          findNewWord["deleted"] = false;
-          findNewWord["updated"] = timeStamp;
+          findNewWord['deleted'] = false;
+          findNewWord['updated'] = timeStamp;
           console.log(
-            "updadd-word found ONLY new word, set deleted flag false."
+            'updadd-word found ONLY new word, set deleted flag false.'
           );
           res.locals.statusCode = 201;
           res.locals.newWord = newWord;
@@ -107,7 +116,7 @@ async function updaddWord(req, res, next) {
             owner: owner,
           });
           console.log(
-            "updadd-word found NEITHER word nor new word, addedItem is:",
+            'updadd-word found NEITHER word nor new word, addedItem is:',
             addedItem
           );
           res.locals.statusCode = 201;
@@ -116,7 +125,7 @@ async function updaddWord(req, res, next) {
       }
     }
   } catch (error) {
-    console.log("updadd-word caught an error:", error);
+    console.log('updadd-word caught an error:', error);
     next(error);
   }
 }
