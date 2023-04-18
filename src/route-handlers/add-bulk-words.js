90d5fca47b22246157235057cebdd0a1ee06c69f
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-17 21:35:34 -0700

    linted, debugged add-bulkk-words

--- src/route-handlers/add-bulk-words.js
+++ src/route-handlers/add-bulk-words.js
@@ -1,57 +1,62 @@
-const LingoWord = require("../models/lingowordModel");
-const timeStamper = require("../utils/time-stamper");
-const { checkString } = require("../utils/validate-inputs");
+const LingoWord = require('../models/lingowordModel');
+const timeStamper = require('../utils/time-stamper');
+const { checkString } = require('../utils/validate-inputs');
 
 async function addBulkWords(req, res, next) {
-  const owner = checkString(req.cookies["useruuid"]);
+  const owner = checkString(res.locals.presenterUuid);
   const { category, words } = req.body;
 
-  if (typeof category !== "string" || category.length < 2) {
+  if (typeof category !== 'string' || category.length < 2) {
     res.locals.statusCode = 400;
-    next("Category is empty or invalid");
+    next('Category is empty or invalid');
   } else {
-    // todo: find end of if-else code block
-
-    const regex = / /g;
-    const cleansedWordList = words.replace(regex, "");
-    const bulkWordArr = cleansedWordList.split(",");
+    const bulkWordArr = words.split(',');
 
     if (!Array.isArray(bulkWordArr)) {
       res.locals.statusCode = 400;
-      next("Words not comma separated");
+      next('Words not comma separated');
     } else {
-
-      console.log("addBulkWords bulkWordArr, size:", bulkWordArr, bulkWordArr.length);
-      if (bulkWordArr.length < 24) {
+      console.log(
+        'addBulkWords bulkWordArr, size:',
+        bulkWordArr,
+        bulkWordArr.length
+      );
+      if (bulkWordArr.length < 4) {
         res.locals.statusCode = 400;
-        next("Not enough words (need at least 24)");
+        next('Not enough words (need at least 24)');
       } else {
-
-        const inputSet = new Set(bulkWordArr);
+        const setInput = [];
+        bulkWordArr.forEach((word) => {
+          setInput.push(word.trim());
+        });
+        const inputSet = new Set(setInput);
         const resultArray = [];
         const existingLingoWordsInDB = await LingoWord.find({
           owner: owner,
           category: category,
         }).exec();
 
-        console.log('addBulkWords exist in DB count:', existingLingoWordsInDB.length);
+        console.log(
+          'addBulkWords exist in DB count:',
+          existingLingoWordsInDB.length
+        );
 
         existingLingoWordsInDB.forEach((entity) => {
-          if (inputSet.has(entity["word"])) {
-            inputSet.delete(entity["word"]);
+          if (inputSet.has(entity['word'])) {
+            inputSet.delete(entity['word']);
             entity.deleted = false;
             entity.updated = timeStamper();
             entity
               .save()
               .then((result) =>
                 console.log(
-                  "saved entity after changing deleted state to false:",
+                  'saved entity after changing deleted state to false:',
                   result
                 )
               )
               .catch((err) =>
                 console.log(
-                  "caught error changing deleted state to false and saving:",
+                  'caught error changing deleted state to false and saving:',
                   err.message
                 )
               );
@@ -62,6 +67,11 @@ async function addBulkWords(req, res, next) {
 
         inputSet.forEach((inputWord) => {
           const newWord = inputWord.trim();
+          console.log(
+            'addBulkWords inputWord, trimmed inputWord:',
+            inputWord,
+            newWord
+          );
           if (newWord.length > 0) {
             const newEntity = new LingoWord({
               owner: owner,
@@ -72,11 +82,17 @@ async function addBulkWords(req, res, next) {
             resultArray.push(newEntity);
           } else {
             // skip words that are empty strings
-            console.log("addBulkWords: An empty or blankspace String was detected in user input, owner:", owner);
+            console.log(
+              'addBulkWords: An empty or blankspace String was detected in user input, owner:',
+              owner
+            );
           }
         });
 
-        console.log('addBulkWords: Total NEW words to add to DB:', resultArray.length);
+        console.log(
+          'addBulkWords: Total NEW words to add to DB:',
+          resultArray.length
+        );
         // do one bulk async save and return as a Promise for handling
         return LingoWord.insertMany(resultArray);
       }
