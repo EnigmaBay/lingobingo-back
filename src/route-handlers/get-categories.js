4a151559834af1bf794b08b035982ce1d51b8368
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-17 21:36:14 -0700

    linted get-categories

--- src/route-handlers/get-categories.js
+++ src/route-handlers/get-categories.js
@@ -1,21 +1,23 @@
-const LingoWord = require("../models/lingowordModel");
-const { checkString } = require("../utils/validate-inputs");
-const cache = require("../utils/cache");
-const timeStamper = require("../utils/time-stamper");
+const LingoWord = require('../models/lingowordModel');
+const { checkString } = require('../utils/validate-inputs');
+const cache = require('../utils/cache');
+const timeStamper = require('../utils/time-stamper');
 
-async function getCategories(req, res, next) {
-  const uuid = checkString(req.cookies["useruuid"]);
+async function getCategories(req, res) {
+  // const uuid = checkString(req.cookies['useruuid']);
+  const uuid = res.locals.presenterUuid;
+  console.log('getCategories received uuid', uuid);
 
   if (!uuid) {
     return [];
   } else {
     const maxCacheLifetime = process.env.MAX_CACHE_LIFETIME;
-    const key = uuid + "-method-" + req.method + "-path-" + req.path;
+    const key = uuid + '-method-' + req.method + '-path-' + req.path;
 
     if (cache[key] && Date.now() - cache[key].timestamp < maxCacheLifetime) {
-      console.log("get-categories cache HIT");
+      console.log('get-categories cache HIT');
     } else {
-      console.log("get-categories cache MISS");
+      console.log('get-categories cache MISS');
       cache[key] = {};
       cache[key].timestamp = timeStamper();
       cache[key].data = await LingoWord.find(
@@ -23,18 +25,19 @@ async function getCategories(req, res, next) {
           owner: uuid,
           deleted: false,
         },
-        "category"
+        'category'
       )
         .exec()
         .then((response) => {
+          console.log('get-categories find call then(response)->', response);
           const mySet = new Set();
           response.forEach((item) => {
-            mySet.add(item["category"]);
+            mySet.add(item['category']);
           });
           return Array.from(mySet);
         })
         .catch((error) => {
-          console.log("error in getCategories", error.message);
+          console.log('error in getCategories', error.message);
           return [];
         });
     }
