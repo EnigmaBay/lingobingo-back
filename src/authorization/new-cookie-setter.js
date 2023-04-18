001c978dbb841a89ac50e047c39f864e0d7081f3
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-17 21:37:53 -0700

    implemented simplified cookie-setter.

--- src/authorization/new-cookie-setter.js
+++ src/authorization/new-cookie-setter.js
@@ -6,10 +6,12 @@ async function cookieSetter(req, res, next) {
       maxAge: maxCookieAge,
       sameSite: 'Strict',
     });
+    console.log('cookie-setter set cookie username:', res.locals.username);
     res.cookie('useruuid', res.locals.presenterUuid, {
       maxAge: maxCookieAge,
       sameSite: 'Strict',
     });
+    console.log('cookie-setter set cookie useruuid:', res.locals.presenterUuid);
   } catch (error) {
     console.log('cookie-setter threw:', error.message);
     next('Unable to set cookies.');
