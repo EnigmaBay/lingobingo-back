08796402e93725d0d1c9531ad228129d3f76df81
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-17 21:33:54 -0700

    linted cookie-validator.

--- src/authorization/cookie-validator.js
+++ src/authorization/cookie-validator.js
@@ -1,60 +1,64 @@
+const { checkString, validateEncodedArg } = require('../utils/validate-inputs');
+const { validate } = require('../utils/presenter-utils');
+
 async function validateCookies(req, res, next) {
-  const {
-    checkString,
-    validateEncodedArg,
-  } = require("../utils/validate-inputs");
-  res.locals.cookieResult = "Unauthorized";
+  res.locals.cookieResult = 'Unauthorized';
+
+  console.log('cookie-validator all cookies in req', req.cookies);
 
   const cookiesDefined =
-    validateEncodedArg(req.cookies["useruuid"]) &&
-    validateEncodedArg(req.cookies["username"]);
+    validateEncodedArg(req.cookies['useruuid']) &&
+    validateEncodedArg(req.cookies['username']);
 
   console.log(
-    "validateCookies ran validateEncodedArg (useruuid, username) and received:",
+    'validateCookies ran validateEncodedArg (useruuid, username) and received:',
     cookiesDefined
+  );
+  console.log(
+    'validateCookies useruuid, username from cookies',
+    req.cookies['useruuid'],
+    req.cookies['username']
   );
 
   try {
     if (cookiesDefined) {
-      console.log("cookie-validator cookiesDefined was true.");
-      const username = checkString(req.cookies["username"]);
-      const { validate } = require("../utils/presenter-utils");
-      const presenter = await validate(req.cookies["useruuid"]);
+      const username = checkString(req.cookies['username']);
+      const presenter = await validate(req.cookies['useruuid']);
       console.log(
-        "cookie-validator: validate(presenteruuid) returned:",
+        'cookie-validator: validate(presenteruuid) returned:',
         presenter
       );
 
-      if (presenter["uuid"] === req.cookies["useruuid"]) {
-        const presenterUuid = presenter["uuid"];
+      if (presenter['uuid'] === req.cookies['useruuid']) {
+        const presenterUuid = presenter['uuid'];
 
-        res.cookie("username", username, {
+        res.cookie('username', username, {
           maxAge: process.env.MAX_COOKIE_AGE,
-          sameSite: "Strict",
+          sameSite: 'Strict',
         });
-        console.log("cookie-validator username cookie set!");
+        console.log('cookie-validator username cookie set!');
 
-        res.cookie("useruuid", presenterUuid, {
+        res.cookie('useruuid', presenterUuid, {
           maxAge: process.env.MAX_COOKIE_AGE,
-          sameSite: "Strict",
+          sameSite: 'Strict',
         });
-        console.log("cookie-validator uuid cookie set!");
-        res.locals.cookieResult = "Authorized";
+        console.log('cookie-validator uuid cookie set!');
+        res.locals.cookieResult = 'Authorized';
       } else {
-        console.log("Cookie Validator: Invalid cookies in request.");
+        console.log('Cookie Validator: Invalid cookies in request.');
         res.locals.statusCode = 401;
-        next("Invalid cookies in request.");
+        next('Invalid cookies in request.');
       }
     } else {
-      console.log("Cookie Validator: Unrecognized cookies in request.");
+      console.log('Cookie Validator: Unrecognized cookies in request.');
       res.locals.statusCode = 401;
-      next("Unrecognized cookies in request.");
+      next('Unrecognized cookies in request.');
     }
   } catch (err) {
-    console.log("cookie-validator threw", err.message);
-    res.locals.cookieResult = "Unauthorized";
+    console.log('cookie-validator threw', err.message);
+    res.locals.cookieResult = 'Unauthorized';
     res.locals.statusCode = 500;
-    next("err.message");
+    next('err.message');
   }
   next();
 }
