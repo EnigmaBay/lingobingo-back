10c99cac05d148081674a03a7fd91f79ec224e27
Jon Rumsey <jon.rumsey@gmail.com>
2023-05-07 17:16:31 -0700

    clarified console log text.

--- src/authorization/decode-auth-params.js
+++ src/authorization/decode-auth-params.js
@@ -1,22 +1,21 @@
 const { Buffer } = require('node:buffer');
 
 async function decodeAuthParams(req, res, next) {
-  console.log('decodeAuthParams received', req.params);
+  console.log('decodeAuthParams received params (aka payload)', req.params);
   const payload = req.params.payload;
-  console.log('received encodedPayload', payload);
   const decodedPayload = Buffer.from(payload, 'base64url').toString('utf8');
   // nickname, email, username, emailVerified
   const payloadArr = decodedPayload.split(',');
-  console.log('payloadArr', payloadArr);
+  console.log('decodeAuthParams payloadArr is', payloadArr);
   res.locals.nickname = payloadArr[0];
   res.locals.email = payloadArr[1];
   res.locals.username = payloadArr[2];
   if (payloadArr[3] !== 'true') {
-    console.log('verification failed');
+    console.log('decodeAuthParams: verification failed');
     next('Unverified email.');
   } else {
     console.log(
-      'email verification check passed for user',
+      'decodeAuthParams: Email verification check passed for user',
       res.locals.username
     );
     next();
